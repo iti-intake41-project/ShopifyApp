edb191a2c7ab59bdf0529f4a4f593d288fcf7eff
Moataz-Akram <61953238+Moataz-Akram@users.noreply.github.com>
2021-06-05 02:06:17 +0200

    organize data classes

---
+++ ShopifyApp/DataClass/customer.swift
@@ -0,0 +1,38 @@
+//
+//  customer.swift
+//  ShopifyApp
+//
+//  Created by Moataz on 05/06/2021.
+//
+
+import Foundation
+
+struct NewCustomer: Codable {
+    let customer: Customer
+}
+
+struct Customer: Codable {
+    let first_name, last_name, email, phone, tags: String?
+    let id: Int?
+    let verified_email: Bool?
+    let addresses: [Address]?
+}
+
+struct Address: Codable {
+    let address, city, province, phone: String?
+    let zip, last_name, first_name, country: String?
+}
+struct LoginResponse: Codable {
+    let customers: [Customer]
+}
+
+extension Encodable {
+    func asDictionary() throws -> [String: Any] {
+        let data = try JSONEncoder().encode(self)
+        guard let dictionary = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any] else {
+            throw NSError()
+        }
+        return dictionary
+    }
+}
+
