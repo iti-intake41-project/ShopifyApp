4dcfa2e852a588581d8b67d678a97603fe48e101
AbanobWadie <abanob.wadi3@gmail.com>
2021-06-16 17:58:51 +0200

    me

--- ShopifyApp/Views/ViewControllers/Me/WishListCollectionViewCell.swift
+++ ShopifyApp/Views/ViewControllers/Me/WishListCollectionViewCell.swift
@@ -9,7 +9,15 @@ import UIKit
 
 class WishListCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var productImg: UIImageView!
-    
     @IBOutlet weak var cardIcon: UIButton!
     @IBOutlet weak var productLbl: UILabel!
+    
+    @IBOutlet weak var imageHeight: NSLayoutConstraint!
+    @IBOutlet weak var imageWidth: NSLayoutConstraint!
+        
+    @IBAction func toggleCart(_ sender: UIButton) {
+
+        
+
+    }
 }
