7f8c228c48f8203ece0de26a688bce73fb3e3036
AbanobWadie <abanob.wadi3@gmail.com>
2021-06-21 12:34:43 +0200

    fix bug

--- ShopifyApp/Views/ViewControllers/Setting/SettingTableViewController.swift
+++ ShopifyApp/Views/ViewControllers/Setting/SettingTableViewController.swift
@@ -37,7 +37,7 @@ class SettingTableViewController: UITableViewController {
               address = settingViewModel.getAddress(appDelegate: &appDelegate)
                
         addrbtn.setTitle(settingViewModel.address?.address1, for: .normal)
-               countrybtn.setTitle(settingViewModel.address?.country, for: .normal)
+               //countrybtn.setTitle(settingViewModel.address?.country, for: .normal)
            
         isLoggedIn = settingViewModel.isLoggedIn()
         if !isLoggedIn {
@@ -74,7 +74,7 @@ class SettingTableViewController: UITableViewController {
         self.address = address
         addrbtn.setTitle(address.address1
             , for: .normal)
-        countrybtn.setTitle(address.country, for: .normal)
+        //countrybtn.setTitle(address.country, for: .normal)
         
         
         
@@ -154,7 +154,7 @@ class SettingTableViewController: UITableViewController {
         let alert = UIAlertController(title: "Alert", message: "Do You  want Logout from application ?", preferredStyle: .alert)
         let yesAction = UIAlertAction(title: "Yes", style: .default) {[weak self] (UIAlertAction) in
             self?.addrbtn.setTitle("", for: .normal)
-            self?.countrybtn.setTitle("", for: .normal)
+            //self?.countrybtn.setTitle("", for: .normal)
             self?.settingViewModel.logout(appDelegate: &self!.appDelegate)
             self?.logoutBtn.isHidden = true
             self?.viewWillAppear(true)
